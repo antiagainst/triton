a34e19118786c33343e8e445f1697cd8af601057
Lei Zhang <antiagainst@gmail.com>
2025-01-13 06:36:04 +0000

    [AMD] Specialize mxfp4 to bf16 upcast

--- third_party/amd/lib/TritonAMDGPUToLLVM/UpcastMXFPToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/UpcastMXFPToLLVM.cpp
@@ -2,6 +2,7 @@
 
 #include "Utility.h"
 #include "mlir/Conversion/LLVMCommon/Pattern.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "mlir/IR/ValueRange.h"
@@ -19,6 +20,129 @@ using namespace mlir::triton;
 using namespace mlir::triton::gpu;
 
 namespace {
+
+SmallVector<Value, 4> createInlineAsmUpcast(RewriterBase &rewriter,
+                                            UpcastMXFPOp upcastOp, Value vI32) {
+  Location loc = upcastOp.getLoc();
+
+  Value allEM = and_(vI32, i32_val(0x77777777));
+  Value allS = and_(vI32, i32_val(0x88888888));
+  // 0xXXXXXXX[X] -> 0x0000000X
+  Value v0EM = and_(allEM, i32_val(0x0000000f));
+  Value v0S = lshr(and_(allS, i32_val(0x0000000f)), i32_val(3));
+  // 0xXXXXXX[X]X -> 0x00000X00
+  Value v1EM = shl(and_(allEM, i32_val(0x000000f0)), i32_val(4));
+  Value v1S = shl(and_(allS, i32_val(0x000000f0)), i32_val(4 - 3));
+  // 0xXXXXX[X]XX -> 0x000X0000
+  Value v2EM = shl(and_(allEM, i32_val(0x00000f00)), i32_val(8));
+  Value v2S = shl(and_(allS, i32_val(0x00000f00)), i32_val(8 - 3));
+  // 0xXXXX[X]XXX -> 0x0X000000
+  Value v3EM = shl(and_(allEM, i32_val(0x0000f000)), i32_val(12));
+  Value v3S = shl(and_(allS, i32_val(0x0000f000)), i32_val(12 - 3));
+  // 0xXXX[X]XXXX -> 0x0000000X
+  Value v4EM = lshr(and_(allEM, i32_val(0x000f0000)), i32_val(16));
+  Value v4S = lshr(and_(allS, i32_val(0x000f0000)), i32_val(16 + 3));
+  // 0xXX[X]XXXXX -> 0x00000X00
+  Value v5EM = lshr(and_(allEM, i32_val(0x00f00000)), i32_val(12));
+  Value v5S = lshr(and_(allS, i32_val(0x00f00000)), i32_val(12 + 3));
+  // 0xX[X]XXXXXX -> 0x000X0000
+  Value v6EM = lshr(and_(allEM, i32_val(0x0f000000)), i32_val(8));
+  Value v6S = lshr(and_(allS, i32_val(0x0f000000)), i32_val(8 + 3));
+  // 0x[X]XXXXXXX -> 0x0X000000
+  Value v7EM = lshr(and_(allEM, i32_val(0xf0000000)), i32_val(4));
+  Value v7S = lshr(and_(allS, i32_val(0xf0000000)), i32_val(4 + 3));
+
+  Value v3210EMIndex = or_(v3EM, or_(v2EM, or_(v1EM, v0EM)));
+  Value v3210SIndex = or_(v3S, or_(v2S, or_(v1S, v0S)));
+  Value v7654EMIndex = or_(v7EM, or_(v6EM, or_(v5EM, v4EM)));
+  Value v7654SIndex = or_(v7S, or_(v6S, or_(v5S, v4S)));
+
+  // FP4 has 4 bits: S.EE.M. Bf16 bit patterns for positive values:
+  //
+  // FP4    | BF16   | Value
+  // ------ | ------ | -----
+  // 0.00.0 | 0x0000 | + 0.0
+  // 0.00.1 | 0x3f00 | + 0.5
+  // 0.01.0 | 0x3f80 | + 1.0
+  // 0.01.1 | 0x3fc0 | + 1.5
+  // 0.10.0 | 0x4000 | + 2.0
+  // 0.10.1 | 0x4040 | + 3.0
+  // 0.11.0 | 0x4080 | + 4.0
+  // 0.11.1 | 0x40c0 | + 6.0
+  Value resB10Lo = i32_val(0xc0800000);
+  Value resB10Hi = i32_val(0xc0804000);
+  Value resB32LoNoS = i32_val(0x3f3f3f00);
+  Value resB32HiNoS = i32_val(0x40404040);
+
+  Value resB32LoS = i32_val(0x8000);
+  Value resB32HiS = i32_val(0);
+
+  Type i32Ty = rewriter.getI32Type();
+  auto permU32FnTy = LLVM::LLVMFunctionType::get(i32Ty, {i32Ty, i32Ty, i32Ty});
+  LLVM::LLVMFuncOp funcOp = appendOrGetExternFuncOp(
+      rewriter, upcastOp, "llvm.amdgcn.perm", permU32FnTy);
+
+  auto v3210ResB10 = LLVM::createLLVMCallOp(rewriter, loc, funcOp,
+                                            {resB10Hi, resB10Lo, v3210EMIndex});
+  auto v3210ResB32NoS = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp, {resB32HiNoS, resB32LoNoS, v3210EMIndex});
+  auto v3210ResB32S = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp, {resB32HiS, resB32LoS, v3210EMIndex});
+  Value v3210ResB32 = or_(v3210ResB32NoS.getResult(), v3210ResB32S.getResult());
+
+  auto v7654ResB10 = LLVM::createLLVMCallOp(rewriter, loc, funcOp,
+                                            {resB10Hi, resB10Lo, v7654EMIndex});
+  auto v7654ResB32NoS = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp, {resB32HiNoS, resB32LoNoS, v7654EMIndex});
+  auto v7654ResB32S = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp, {resB32HiS, resB32LoS, v7654EMIndex});
+  Value v7654ResB32 = or_(v7654ResB32NoS.getResult(), v7654ResB32S.getResult());
+
+  auto r10 = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp,
+      {v3210ResB32, v3210ResB10.getResult(), i32_val(0x05010400)});
+  auto r32 = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp,
+      {v3210ResB32, v3210ResB10.getResult(), i32_val(0x07030602)});
+
+  auto r54 = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp,
+      {v7654ResB32, v7654ResB10.getResult(), i32_val(0x05010400)});
+  auto r76 = LLVM::createLLVMCallOp(
+      rewriter, loc, funcOp,
+      {v7654ResB32, v7654ResB10.getResult(), i32_val(0x07030602)});
+
+  return {r10.getResult(), r32.getResult(), r54.getResult(), r76.getResult()};
+}
+
+SmallVector<Value> upcast8xMxfp4ToBf16(RewriterBase &rewriter,
+                                       UpcastMXFPOp upcastOp,
+                                       ArrayRef<Value> values) {
+  Location loc = upcastOp.getLoc();
+  SmallVector<Value> results;
+  MLIRContext *ctx = rewriter.getContext();
+  assert(values.size() % 4 == 0);
+  for (int i = 0; i < values.size(); i += 4) {
+    Value v0 = values[i];
+    Value v1 = values[i + 1];
+    Value v2 = values[i + 2];
+    Value v3 = values[i + 3];
+    Value packedVec = undef(vec_ty(i8_ty, 4));
+    packedVec = insert_element(packedVec, v0, i32_val(0));
+    packedVec = insert_element(packedVec, v1, i32_val(1));
+    packedVec = insert_element(packedVec, v2, i32_val(2));
+    packedVec = insert_element(packedVec, v3, i32_val(3));
+    SmallVector<Type> rets(4, i32_ty);
+    SmallVector<Value, 4> ret =
+        createInlineAsmUpcast(rewriter, upcastOp, bitcast(packedVec, i32_ty));
+    for (int i = 0; i < 4; i++) {
+      Value vecbf16 = bitcast(ret[i], vec_ty(bf16_ty, 2));
+      results.push_back(extract_element(vecbf16, i32_val(0)));
+      results.push_back(extract_element(vecbf16, i32_val(1)));
+    }
+  }
+  return results;
+}
 
 SmallVector<Value> convertMxfp4x2ToFp16x2(RewriterBase &rewriter, Location loc,
                                           ArrayRef<Value> values) {
@@ -142,7 +266,7 @@ public:
     bool useFp16 = op.getType().getElementType().isF16();
     if (isPacked) {
       xVals = useFp16 ? convertMxfp4x2ToFp16x2(rewriter, loc, xVals)
-                      : LLVM::convertMxfp4x2ToBf16x2(rewriter, loc, xVals);
+                      : upcast8xMxfp4ToBf16(rewriter, op, xVals);
     }
 
     // Given that MFMA layout for the A tensor arranges thread in a column-major
