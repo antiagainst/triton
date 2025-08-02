1b4597d4ded2f617ed2ada843e8bb108395a3f4a
Lei Zhang <antiagainst@gmail.com>
2025-08-02 17:23:41 +0000

    [WIP] enable both fp16 and bf16

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -101,6 +101,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::registerTritonAMDGPUInsertInstructionSchedHints();
   mlir::triton::registerTritonAMDGPULowerInstructionSchedHints();
   mlir::registerTritonAMDFoldTrueCmpI();
+  mlir::registerTritonAMDPeepholeOptimize();
 
   // NVWS passes
   mlir::triton::registerNVWSTransformsPasses();
--- third_party/amd/lib/TritonAMDGPUDialectToLLVM/PeepholePatterns.cpp
+++ third_party/amd/lib/TritonAMDGPUDialectToLLVM/PeepholePatterns.cpp
@@ -6,12 +6,46 @@
 #include "mlir/IR/PatternMatch.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "llvm/ADT/TypeSwitch.h"
 
 using namespace mlir;
 using namespace mlir::triton;
-using namespace mlir::triton::gpu;
+namespace tt = mlir::triton;
+namespace ttg = mlir::triton::gpu;
 
 namespace {
+
+// Goes through the RHS of mul op to figure out the BF16/FP32 scale value.
+TypedValue<RankedTensorType> getUpcastFp32Scale(arith::MulFOp mulOp) {
+  Value scale = mulOp.getRhs();
+  auto scaleTy = dyn_cast<RankedTensorType>(scale.getType());
+  if (!scaleTy)
+    return {};
+
+  // First check whether the scale is encoded as a BF16 value.
+  if (scaleTy.getElementType().isBF16())
+    return cast<TypedValue<RankedTensorType>>(scale);
+
+  // Otherwise, for FP16 case, we need to figure out the original FP32 value.
+  if (!scaleTy.getElementType().isF16())
+    return {};
+  // Go through various shape manipulation ops.
+
+  arith::TruncFOp truncOp = nullptr;
+  while (scale) {
+    llvm::TypeSwitch<Operation *>(scale.getDefiningOp())
+        .Case<tt::BroadcastOp, tt::ExpandDimsOp, tt::ReshapeOp, tt::TransOp>(
+            [&](auto op) { scale = op.getSrc(); })
+        .Case<arith::TruncFOp>([&](auto op) { truncOp = op, scale = nullptr; })
+        .Default([&](auto op) { scale = nullptr; });
+  }
+  if (!truncOp)
+    return {};
+  auto inType = dyn_cast<RankedTensorType>(truncOp.getIn().getType());
+  if (!inType || !inType.getElementType().isF32())
+    return {};
+  return cast<TypedValue<RankedTensorType>>(truncOp.getIn());
+}
 
 class UseScaledUpcastFp4 : public OpRewritePattern<arith::MulFOp> {
 public:
@@ -24,16 +58,16 @@ public:
     if (targetInfo.getISAFamily() != AMD::ISAFamily::CDNA4)
       return failure();
 
-    auto scaleTy = dyn_cast<RankedTensorType>(mulOp.getRhs().getType());
-    if (!scaleTy || !scaleTy.getElementType().isBF16())
-      return failure();
-    auto fp4ToFpOp = mulOp.getLhs().getDefiningOp<Fp4ToFpOp>();
+    auto fp4ToFpOp = mulOp.getLhs().getDefiningOp<ttg::Fp4ToFpOp>();
     if (!fp4ToFpOp)
       return failure();
 
+    auto scale = getUpcastFp32Scale(mulOp);
+    if (!scale)
+      return failure();
+
     rewriter.replaceOpWithNewOp<amdgpu::ScaledUpcastFp4Op>(
-        mulOp, mulOp.getType(), fp4ToFpOp.getSrc(), mulOp.getRhs(),
-        fp4ToFpOp.getAxis());
+        mulOp, mulOp.getType(), fp4ToFpOp.getSrc(), scale, fp4ToFpOp.getAxis());
     return success();
   }
 
@@ -52,15 +86,16 @@ public:
     if (targetInfo.getISAFamily() != AMD::ISAFamily::CDNA4)
       return failure();
 
-    auto scaleTy = dyn_cast<RankedTensorType>(mulOp.getRhs().getType());
-    if (!scaleTy || !scaleTy.getElementType().isBF16())
-      return failure();
     auto fpToFpOp = mulOp.getLhs().getDefiningOp<FpToFpOp>();
     if (!fpToFpOp)
       return failure();
 
+    auto scale = getUpcastFp32Scale(mulOp);
+    if (!scale)
+      return failure();
+
     rewriter.replaceOpWithNewOp<amdgpu::ScaledUpcastFp8Op>(
-        mulOp, mulOp.getType(), fpToFpOp.getSrc(), mulOp.getRhs());
+        mulOp, mulOp.getType(), fpToFpOp.getSrc(), scale);
     return success();
   }
 
@@ -80,4 +115,3 @@ void populatePeepholeOptimizationPatterns(RewritePatternSet &patterns,
 }
 
 } // namespace mlir::triton::AMD
-
